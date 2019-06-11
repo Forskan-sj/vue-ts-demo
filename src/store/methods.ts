365500a7766ea2b627ad1acb7f9d950ddce4fc68
盛杰 <2324676536@qq.com>
2019-06-11 13:54:51 +0800

    1

--- src/store/methods.ts
+++ src/store/methods.ts
@@ -23,3 +23,59 @@ export const setUserVal = (val: string) => {
     console.log('user/setName error' + e);
   })
 }
+
+// let s: number = 3
+// let ss: string = 'sd'
+// s ='ss'
+
+interface Itest {
+  id: number,
+  name?: string,
+  avatar?: string
+}
+let info: Itest 
+// info.
+let infotemp = {
+  id: 1,
+  name: '22'
+}
+let infotemp1 = {
+  id: 1
+}
+info = infotemp
+info = infotemp1
+
+
+class Student {
+  fullName: string;
+  constructor(public firstName:string, public middleInitial:string, public lastName:string) {
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+  private getA(): void {
+
+  }
+  protected getB(): void {
+
+  }
+  public getFN(par: number): string;
+  public getFN(par: string): string;
+  public getFN(par: any): string {
+    return this.fullName
+  }
+  public getSN(par: number) {
+    return this.middleInitial + this.lastName
+  } 
+}
+
+let user = new Student('sdf', 'asdf', '')
+user.getFN(2)
+
+
+function gets(par1: string, par2: number, par3: number[], par4: Itest) {
+  console.log('object');
+}
+// gets('1', 2, [3], 4)
+// Math.pow(1, 2)
+
+
+// 拥有函数和类的目的，是在不知道如何实现它们的情况下使用它们。换句话说，我们应该依赖于他们的接口，而不是他们的实现。
